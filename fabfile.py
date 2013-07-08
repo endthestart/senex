9c5df08eba6ac1ff4709847ff4dbe3ad55bedec7
Michael Anderson <andermic@gmail.com>
2013-07-07 19:55:48 -0500

    Add build_commit command to fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,27 @@
-from fabric.api import lcd, local
+from fabric.api import *
+from fabric.colors import green, red
+
+def build_commit(warn_only=True):
+    """Build a commit"""
+    local_branch = prompt("checkout branch: ")
+    rebase_branch = prompt("rebase branch: ")
+
+    local('git checkout %s' % local_branch)
+    local('git add -p')
+    local('git add -u')
+
+    message = prompt("commit message: ")
+
+    local('git commit -m %s' % message)
+    local('git checkout %s' % rebase_branch)
+    local('git pull origin %s' % rebase_branch)
+    local('git checkout %s' % local_branch)
+    local('git rebase %s' % rebase_branch)
+    local('git checkout %s' % rebase_branch)
+    local('git merge %s' % local_branch)
+    local('git push origin %s' % rebase_branch)
+    local('git checkout %s' % local_branch)
+
 
 def prepare_deployment(branch_name):
     local('python manage.py test senex')
