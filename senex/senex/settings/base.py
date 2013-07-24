c99091f6225ccbcfe88744de2f7abf3f375ed844
Michael Anderson <andermic@gmail.com>
2013-07-23 20:31:47 -0500

    Added new apps and database settings.

--- senex/senex/settings/base.py
+++ senex/senex/settings/base.py
@@ -25,7 +25,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
         "NAME": "",
         "USER": "",
         "PASSWORD": "",
@@ -74,7 +74,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = normpath(join(SITE_ROOT, 'staticfiles'))
+STATIC_ROOT = normpath(join(SITE_ROOT, '../static'))
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -143,6 +143,8 @@ DJANGO_APPS = (
 
 THIRD_PARTY_APPS = (
     #'south',
+    'django_localflavor_us',
+    'Pillow',
 )
 
 LOCAL_APPS = (
