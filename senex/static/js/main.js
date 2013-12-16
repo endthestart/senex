bc10dfa223f20627bd7a32c65618d07c190a0ee5
Michael Anderson <andermic@gmail.com>
2013-12-15 18:57:38 -0600

    Adjust styling of product customization page. Remove bootstrap js files.

---
+++ senex/static/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+    CustomForm.init();
+});
+
+var CustomForm = {
+    init: function() {
+        this.submitButton = $(".addcart");
+        this.customForm = $("#product-options-form");
+
+        this.bind();
+    },
+
+    bind: function() {
+        var self = this;
+
+        self.submitButton.click(function(e) {
+            e.preventDefault()
+            console.log("awesome");
+            CustomForm.customForm.submit();
+        });
+    }
+
+};
