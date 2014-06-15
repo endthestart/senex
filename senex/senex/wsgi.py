f66afd77a5358cf6182a40760c3824ccde0e58f9
Michael Anderson <andermic@gmail.com>
2014-06-14 18:39:49 -0600

    Add new relic support.

--- senex/senex/wsgi.py
+++ senex/senex/wsgi.py
@@ -21,6 +21,7 @@ import os
 # os.environ["DJANGO_SETTINGS_MODULE"] = "senex.settings"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "senex.settings")
 
+
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
@@ -28,6 +29,6 @@ from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
+import newrelic.agent
+newrelic.agent.initialize('/srv/www/newrelic/senex.newrelic.ini')
+application = newrelic.agent.wsgi_application()(application)
