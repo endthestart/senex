6ba56eb5cd3997b02ec45b2a085bf5d61642ed83
Michael Anderson <andermic@gmail.com>
2014-08-18 11:49:20 -0600

    Add user registration emails.

---
+++ senex/senex/signals.py
@@ -0,0 +1,17 @@
+from django.db.models.signals import post_save
+from django.contrib.auth import get_user_model
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+
+
+def send_registration_email(sender, **kwargs):
+    user = kwargs['instance']
+    text_template = 'senex_shop/email/user_registration_body.txt'
+    subject_template = 'senex_shop/email/user_registration_subject.txt'
+    context = {'user': user}
+    email_text = render_to_string(text_template, context)
+    email_subject = render_to_string(subject_template, context)
+    if kwargs['created']:
+        send_mail(email_subject, email_text, 'info@senexcycles.com', [user.email])
+
+post_save.connect(send_registration_email, sender=get_user_model())
