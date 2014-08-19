fe4308cadf902e56e7eee214234f134dde9a70be
Michael Anderson <andermic@gmail.com>
2014-08-19 11:28:27 -0600

    Add simple product page for products without customization.

--- senex/senex/settings/production.py
+++ senex/senex/settings/production.py
@@ -25,7 +25,7 @@ STATIC_URL = '/static/'
 # See: https://docs.djangoproject.com/en/dev/topics/email/
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'mail.senexcycles.com'
-EMAIL_PORT = '465'
+EMAIL_PORT = '587'
 EMAIL_HOST_USER = 'info@senexcycles.com'
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
 EMAIL_USE_TLS = False
